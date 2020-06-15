ffc81a50a9b9bc2e1653cf6298f34919bf2e2649
Ian Kropp <iankropp@gmail.com>
2020-06-15 16:52:28 -0400

    Switched over to just using the input_sub module for cleanliness

--- runPy.py
+++ runPy.py
@@ -7,25 +7,11 @@ filex = "UFGA8201.MZX"
 
 os.chdir(path)
 
-# 
-rawout = dssat4py.readfilex(path, filex, 2)
+dssat4py.readfilex(path, filex, 4)
 
-int_vals = rawout[0]
-int_names_raw = rawout[1]
-
-
-int_names = []
-
-for row in range(np.size(int_names_raw, 0)): 
-    string = reduce((lambda x, y: x + y ), int_names_raw[row,:]).decode("ascii")
-    int_names.append(string.strip())
-
-exp_vars = {}
-
-for ivar in range(len(int_vals)):
-    exp_vars[int_names[ivar]] = int_vals[ivar]
-    
-print(exp_vars)
 
 dssat4py.printtest()
+
+# 
+#rawout = dssat4py.readfilex(path, filex, 2)
 
--- src/dssat4py.for
+++ src/dssat4py.for
@@ -16,7 +16,7 @@ C               (SwitchType) is defined in ModuleDefs.for.
 C
 
       SUBROUTINE READFILEX(
-     &    PATHEX, FILEX, TRTNUM, IVARVALS, IVARNAMS)
+     &    PATHEX, FILEX, TRTNUM)
 
       USE ModuleData
       USE ModuleDefs
@@ -26,36 +26,15 @@ C
       INCLUDE 'COMSWI.blk'
       INCLUDE 'COMIBS.blk'
 
-C     Output variables
-
-      INTEGER,      dimension(27) :: IVARVALS
-      CHARACTER*6,  dimension(27) :: IVARNAMS
-
 C     Input variables
 
-      CHARACTER*  1 WMODI, RNMODE
-      CHARACTER*  2 CROP
-      CHARACTER*  6 VARNO
-      CHARACTER*  8 MODEL, MODELARG
-      CHARACTER* 10 SLNO ! this had no declaration in input_sub, but I
-C     found a d declaration in a different file for 10 char
-      CHARACTER* 12 FILEX
-      CHARACTER* 25 TITLET
-      CHARACTER* 42 CHEXTR(NAPPL) !declared as CHEXTR(NAPPL) in inputub
-      CHARACTER* 80 PATHEX
-      CHARACTER* 92 FILEX_P
+      CHARACTER*1   RNMODE
+      CHARACTER*8   MODELARG
+      CHARACTER*12  FILEX   
+      CHARACTER*30  FILEIO
+      CHARACTER*80  PATHEX
       CHARACTER*120 FILECTL
-
-      CHARACTER*9 WSTAT
-
-      INTEGER RUN,NYRS,FROP,TRTN,EXPP,EXPN,TRTALL,FTYPEN,NFORC,NDOF,PMTYPE
-      INTEGER LNSIM,LNCU,LNHAR,LNTIL,LNCHE,LNFLD,LNIC,LNPLT,LNIR
-      INTEGER LNFER,LNRES,ROTNUM,TRTNUM,LNENV,LNSA,REPNO
-      INTEGER IIRV(NAPPL)  ! this is declared as IIRV(NAPPL) in input_sub
-
-      LOGICAL UseSimCtr
-
-      REAL PLTFOR
+      INTEGER ROTNUM, RUN, TRTNUM, LUNIO, REPNO
 
       TYPE (ControlType) CONTROL
       TYPE (SwitchType)  ISWITCH
@@ -66,10 +45,9 @@ C     found a d declaration in a different file for 10 char
       REPNO   = 1
       MEWTH   = 'M'
 
-      FILEX_P = TRIM(PATHEX)//FILEX
-      CALL Join_Trim(PATHEX, FILEX, FILEX_P)
+C      DSSATP = TRIM(PATHEX)//'DSSATPRO.L47'
 
-      DSSATP = TRIM(PATHEX)//'DSSATPRO.L47'
+
 
       CONTROL % REPNO = REPNO
       CONTROL % RUN = RUN
@@ -82,76 +60,15 @@ C     found a d declaration in a different file for 10 char
 
       CALL PUT(CONTROL)
 
+      CALL GETLUN('FILEIO', LUNIO)
+      FILEIO = 'DSSAT47.INP'
 
-       CALL IPEXP (MODEL, RUN, RNMODE, FILEX,PATHEX,FILEX_P, FILECTL,
-     &     SLNO,NYRS,VARNO,CROP,WMODI,
-     &     FROP,TRTN,EXPP,EXPN,TITLET,TRTALL,TRTNUM,ROTNUM, 
-     &     IIRV,FTYPEN,CHEXTR,NFORC,PLTFOR,NDOF,PMTYPE,
-     &     LNSIM,LNCU,LNHAR,LNENV,LNTIL,LNCHE,
-     &     LNFLD,LNSA,LNIC,LNPLT,LNIR,LNFER,LNRES, 
-     &     CONTROL, ISWITCH, UseSimCtr, MODELARG)
-
-
-      print *, '---'
-      print *, WSTA
-      print *, '---'
-
-      IVARVALS(1) = RUN
-      IVARVALS(2) = NYRS
-      IVARVALS(3) = FROP
-      IVARVALS(4) = TRTN
-      IVARVALS(5) = EXPP
-      IVARVALS(6) = EXPN
-      IVARVALS(7) = TRTALL
-      IVARVALS(8) = FTYPEN
-      IVARVALS(9) = NFORC
-      IVARVALS(10) = NDOF
-      IVARVALS(11) = PMTYPE
-      IVARVALS(12) = LNSIM
-      IVARVALS(13) = LNCU
-      IVARVALS(14) = LNHAR
-      IVARVALS(15) = LNTIL
-      IVARVALS(16) = LNCHE
-      IVARVALS(17) = LNFLD
-      IVARVALS(18) = LNIC
-      IVARVALS(19) = LNPLT
-      IVARVALS(20) = LNIR
-      IVARVALS(21) = LNFER
-      IVARVALS(22) = LNRES
-      IVARVALS(23) = ROTNUM
-      IVARVALS(24) = TRTNUM
-      IVARVALS(25) = LNENV
-      IVARVALS(26) = LNSA
-      IVARVALS(27) = REPNO
+        CALL INPUT_SUB(
+     &    FILECTL, FILEIO, FILEX, MODELARG, PATHEX,       !Input
+     &    RNMODE, ROTNUM, RUN, TRTNUM,                    !Input
+     &    ISWITCH, CONTROL)                               !Output
 
 
-      IVARNAMS(1) = "RUN"
-      IVARNAMS(2) = "NYRS"
-      IVARNAMS(3) = "FROP"
-      IVARNAMS(4) = "TRTN"
-      IVARNAMS(5) = "EXPP"
-      IVARNAMS(6) = "EXPN"
-      IVARNAMS(7) = "TRTALL"
-      IVARNAMS(8) = "FTYPEN"
-      IVARNAMS(9) = "NFORC"
-      IVARNAMS(10) = "NDOF"
-      IVARNAMS(11) = "PMTYPE"
-      IVARNAMS(12) = "LNSIM"
-      IVARNAMS(13) = "LNCU"
-      IVARNAMS(14) = "LNHAR"
-      IVARNAMS(15) = "LNTIL"
-      IVARNAMS(16) = "LNCHE"
-      IVARNAMS(17) = "LNFLD"
-      IVARNAMS(18) = "LNIC"
-      IVARNAMS(19) = "LNPLT"
-      IVARNAMS(20) = "LNIR"
-      IVARNAMS(21) = "LNFER"
-      IVARNAMS(22) = "LNRES"
-      IVARNAMS(23) = "ROTNUM"
-      IVARNAMS(24) = "TRTNUM"
-      IVARNAMS(25) = "LNENV"
-      IVARNAMS(26) = "LNSA"
-      IVARNAMS(27) = "REPNO"
 
       END SUBROUTINE
 
