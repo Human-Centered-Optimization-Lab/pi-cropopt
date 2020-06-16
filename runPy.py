b57a5df75e7b925ea4ea508fe577e5d5156bce7f
Ian Kropp <iankropp@gmail.com>
2020-06-16 12:47:02 -0400

    Moved dssat4py into its own fortran module

--- runPy.py
+++ runPy.py
@@ -7,12 +7,12 @@ filex = "UFGA8201.MZX"
 
 os.chdir(path)
 
-dssat4py.readfilex(path, filex, 4)
+dssat4py.mod.readfilex(path, filex, 4)
 
-dssat4py.setwsta("WXYZ")
+dssat4py.mod.setwsta("WXYZ")
 
-dssat4py.printtest()
-dssat4py.writetempx()
+dssat4py.mod.printtest()
+dssat4py.mod.writetempx()
 
 # 
 #rawout = dssat4py.readfilex(path, filex, 2)
--- src/dssat4py.for
+++ src/dssat4py.for
@@ -19,77 +19,89 @@ C     --------------------------------
 C     READFILEX
 C     --------------------------------
 
-      SUBROUTINE READFILEX(PATHEX, FILEX, TRTNUM)
+      MODULE MOD
 
-      USE ModuleData
-      USE ModuleDefs
-
-      IMPLICIT NONE
-
-      INCLUDE 'COMSWI.blk'
-      INCLUDE 'COMIBS.blk'
-
-C     Input variables
+        USE ModuleData
+        USE ModuleDefs
+       
+        IMPLICIT NONE
 
 
+        INCLUDE 'COMSWI.blk'
+        INCLUDE 'COMIBS.blk'
 
-      INTEGER YRIC,EXPN,FROP,ISENS,LNCHE,LNCU,LNENV,LNSA,LNTIL
-      INTEGER LNFER,LNFLD,LNHAR,LNIC, LNIR,LNPLT,LNRES,LNSIM,NDOF
-      INTEGER NFORC,NYRS,PMTYPE,TRTN,TRTALL
-
-
-      CHARACTER*1000 atline
-      CHARACTER*42 CHEXTR(NAPPL)
-      CHARACTER*16 VRNAME
-      CHARACTER*8 MODEL
-      CHARACTER*6 ECONO, VARNO
-      CHARACTER*2 CROP,PRCROP
-
-      REAL EFINOC, EFNFIX,PLTFOR,WRESND,WRESR
-      REAL INO3(NL),INH4(NL)
-      REAL SWINIT(NL)
+        INTEGER YRIC,EXPN,FROP,ISENS,LNCHE,LNCU,LNENV,LNSA,LNTIL
+        INTEGER LNFER,LNFLD,LNHAR,LNIC, LNIR,LNPLT,LNRES,LNSIM,NDOF
+        INTEGER NFORC,NYRS,PMTYPE,RUN,TRTN,TRTALL
+        INTEGER ROTNUM, TRTNUM, LUNIO, REPNO
 
 
+        CHARACTER*1000 atline
+        CHARACTER*120 FILECTL
+        CHARACTER*80 PATHEX
+        CHARACTER*42 CHEXTR(NAPPL)
+        CHARACTER*30  FILEIO
+        CHARACTER*16 VRNAME
+        CHARACTER*12  FILEX   
+        CHARACTER*8 MODEL, MODELARG
+        CHARACTER*6 ECONO, VARNO
+        CHARACTER*2 CROP,PRCROP
+        CHARACTER*1 RNMODE
 
-      CHARACTER*1   RNMODE
-      CHARACTER*8   MODELARG
-      CHARACTER*12  FILEX   
-      CHARACTER*30  FILEIO
-      CHARACTER*80  PATHEX
-      CHARACTER*120 FILECTL
-      INTEGER ROTNUM, RUN, TRTNUM, LUNIO, REPNO
+        REAL EFINOC, EFNFIX,PLTFOR,WRESND,WRESR
+        REAL INO3(NL),INH4(NL)
+        REAL SWINIT(NL)
 
-      TYPE (ControlType) CONTROL
-      TYPE (SwitchType)  ISWITCH
+        TYPE (ControlType) CONTROL
+        TYPE (SwitchType)  ISWITCH
 
-      RUN     = 1
-      RNMODE  = 'B'
-      ROTNUM  = 0
-      REPNO   = 1
-      MEWTH   = 'M'
+      CONTAINS 
 
-C      DSSATP = TRIM(PATHEX)//'DSSATPRO.L47'
+        SUBROUTINE READFILEX(PATHEX, FILEX, TRTNUM)
+
+        USE ModuleData
+        USE ModuleDefs
+
+        IMPLICIT NONE
+
+        CHARACTER*80 PATHEX
+        CHARACTER*12  FILEX   
+        INTEGER TRTNUM
+
+
+        FILEX = FILEX
+
+C       Input variables
+
+
+        RUN     = 1
+        RNMODE  = 'B'
+        ROTNUM  = 0
+        REPNO   = 1
+        MEWTH   = 'M'
+
+C        DSSATP = TRIM(PATHEX)//'DSSATPRO.L47'
 
 
 
-      CONTROL % REPNO = REPNO
-      CONTROL % RUN = RUN
-      CONTROL % YRDOY = 0
-      CONTROL % FILEX   = FILEX
-      CONTROL % RNMODE  = RNMODE
-      CONTROL % ROTNUM  = ROTNUM
-      CONTROL % TRTNUM  = TRTNUM
-      CONTROL % ERRCODE = 0
+        CONTROL % REPNO = REPNO
+        CONTROL % RUN = RUN
+        CONTROL % YRDOY = 0
+        CONTROL % FILEX   = FILEX
+        CONTROL % RNMODE  = RNMODE
+        CONTROL % ROTNUM  = ROTNUM
+        CONTROL % TRTNUM  = TRTNUM
+        CONTROL % ERRCODE = 0
 
-      CALL PUT(CONTROL)
+        CALL PUT(CONTROL)
 
-      CALL GETLUN('FILEIO', LUNIO)
-      FILEIO = 'DSSAT47.INP'
+        CALL GETLUN('FILEIO', LUNIO)
+        FILEIO = 'DSSAT47.INP'
 
-  
+    
 
-C     TODO in the future, split this into a read function and a
-C     write function 
+C       TODO in the future, split this into a read function and a
+C       write function 
 
         CALL INPUT_SUB(
      &    FILECTL, FILEIO, FILEX, MODELARG, PATHEX,       !Input
@@ -98,98 +110,77 @@ C     write function
 
 
 
-      END SUBROUTINE
+        END SUBROUTINE
 
-C     --------------------------------
-C     PRINTTEST
-C     --------------------------------
-      SUBROUTINE PRINTTEST()
+C       --------------------------------
+C       PRINTTEST
+C       --------------------------------
+        SUBROUTINE PRINTTEST()
 
-      USE ModuleData
-      USE ModuleDefs
+        USE ModuleData
+        USE ModuleDefs
 
-      IMPLICIT NONE
+        IMPLICIT NONE
 
-      INCLUDE 'COMSWI.blk'
-      INCLUDE 'COMIBS.blk'
+        INCLUDE 'COMSWI.blk'
+        INCLUDE 'COMIBS.blk'
 
-      print *, '***'
-      print *, WSTA
-      print *, '***'
-
-
-      END SUBROUTINE
-
-C     --------------------------------
-C     SETWSTA
-C     --------------------------------
-      SUBROUTINE SETWSTA(NWSTA)
-
-      USE ModuleData
-      USE ModuleDefs
-
-      IMPLICIT NONE
-
-      INCLUDE 'COMSWI.blk'
-      INCLUDE 'COMIBS.blk'
-      CHARACTER *4 NWSTA  
-
-      WSTA = NWSTA 
-
-      END SUBROUTINE
-
-C     --------------------------------
-C     WRITETEMPX
-C     --------------------------------
-      SUBROUTINE WRITETEMPX()
-
-      USE ModuleData
-      USE ModuleDefs
-
-      IMPLICIT NONE
-
-      INCLUDE 'COMSWI.blk'
-      INCLUDE 'COMIBS.blk'
+        print *, '***'
+        print *, WSTA
+        print *, '***'
 
 
+        END SUBROUTINE
 
-      INTEGER YRIC,EXPN,FROP,ISENS,LNCHE,LNCU,LNENV,LNSA,LNTIL
-      INTEGER LNFER,LNFLD,LNHAR,LNIC, LNIR,LNPLT,LNRES,LNSIM,NDOF
-      INTEGER NFORC,NYRS,PMTYPE,RUN,TRTN,TRTALL
+C       --------------------------------
+C       SETWSTA
+C       --------------------------------
+        SUBROUTINE SETWSTA(NWSTA)
 
+        USE ModuleData
+        USE ModuleDefs
 
-      CHARACTER*1000 atline
-      CHARACTER* 80 PATHEX
-      CHARACTER*42 CHEXTR(NAPPL)
-      CHARACTER*30  FILEIO
-      CHARACTER*16 VRNAME
-      CHARACTER*12  FILEX   
-      CHARACTER*8 MODEL
-      CHARACTER*6 ECONO, VARNO
-      CHARACTER*2 CROP,PRCROP
-      CHARACTER*1 RNMODE
+        IMPLICIT NONE
 
-      REAL EFINOC, EFNFIX,PLTFOR,WRESND,WRESR
-      REAL INO3(NL),INH4(NL)
-      REAL SWINIT(NL)
-      
-      PRINT *, '8888'
-      print *,  FILEIO
-      print *, VRNAME
-      print *,  FILEX   
-      print *, MODEL
-      print *, ECONO, VARNO
-      print *, CROP,PRCROP
-      print *, RNMODE
-      PRINT *, FILEIO
-      print *, WSTA
-      PRINT *, '8888'
+        INCLUDE 'COMSWI.blk'
+        INCLUDE 'COMIBS.blk'
+        CHARACTER *4 NWSTA  
 
-C        CALL OPTEMPY2K(RNMODE,FILEX,PATHEX,
-C     &            YRIC,PRCROP,WRESR,WRESND,EFINOC,EFNFIX,
-C     &            SWINIT,INH4,INO3,NYRS,VARNO,VRNAME,CROP,MODEL,
-C     &            RUN,FILEIO,EXPN,ECONO,FROP,TRTALL,TRTN,
-C     &            CHEXTR,NFORC,PLTFOR,NDOF,PMTYPE,ISENS)
+        WSTA = NWSTA 
+
+        END SUBROUTINE
+
+C       --------------------------------
+C       WRITETEMPX
+C       --------------------------------
+        SUBROUTINE WRITETEMPX()
+
+        USE ModuleData
+        USE ModuleDefs
+
+        IMPLICIT NONE
+
+        INCLUDE 'COMSWI.blk'
+        INCLUDE 'COMIBS.blk'
+
+        
+        PRINT *, '8888'
+        print *,  FILEIO
+        print *, VRNAME
+        print *, FILEX   
+        print *, MODEL
+        print *, ECONO, VARNO
+        print *, CROP,PRCROP
+        print *, RNMODE
+        PRINT *, FILEIO
+        print *, WSTA
+        PRINT *, '8888'
+
+        CALL OPTEMPY2K(RNMODE,FILEX,PATHEX,
+     &            YRIC,PRCROP,WRESR,WRESND,EFINOC,EFNFIX,
+     &            SWINIT,INH4,INO3,NYRS,VARNO,VRNAME,CROP,MODEL,
+     &            RUN,FILEIO,EXPN,ECONO,FROP,TRTALL,TRTN,
+     &            CHEXTR,NFORC,PLTFOR,NDOF,PMTYPE,ISENS)
       
         CALL OPTEMPXY2K (YRIC,PRCROP,WRESR,WRESND,EFINOC,EFNFIX,
      &           SWINIT,INH4,INO3,NYRS,VARNO,VRNAME,CROP,
@@ -200,4 +191,7 @@ C     &            CHEXTR,NFORC,PLTFOR,NDOF,PMTYPE,ISENS)
 
 
 
-      END SUBROUTINE
+        END SUBROUTINE
+
+      END MODULE mod
+
