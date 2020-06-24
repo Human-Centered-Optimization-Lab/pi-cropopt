bf658c1c95a2151dac1842ae580626148261aac3
Ian Kropp <kroppian@msu.edu>
2020-06-24 15:23:29 -0400

    Have the directory copying set up

--- src/python/dssat4dum.py
+++ src/python/dssat4dum.py
@@ -1,5 +1,10 @@
+from functools import reduce
+from multiprocessing import Pool
+import numpy as np
+from shutil import copytree, copy
+import errno
 
-class dssat4dum():
+class Dssat4Dum():
 
 
     def __init__(self, home, fileio, tmp_dir):
@@ -7,36 +12,51 @@ class dssat4dum():
         self.home = home
         self.fileio = fileio
         self.tmp_dir = tmp_dir
+        self.activeIds = []       
 
-        # Set up run directories 
+    # irrsched is a nx2 matrix representing an irrigation schedule of n apps
 
+    def run(self, irrsched, runid=0):
 
-    def editIrr(self, irrsched):
+        if runid not in self.activeIds: 
+            self._setupDirectory(self.home, self.tmp_dir, runid)
+
+        # self._editIrr(self.fileio, irrsched)
+
+        return 
+      
+    # irrsched is a nx2xy matrix representing j irrigation schedules of n apps
+    def run_batch(self, irrsched, threads=1):
+
+        batch_count = np.size(irrsched, 0)
+
+        # Create the arg list, with the first item being the irrigation 
+        # schedule and the second being the run id
+        args = list(map((lambda a, ind : (a, ind)), irrsched, range(0,batch_count)))
+
+        yields = []
+        with Pool(threads) as p: 
+            yields = p.map(run, args)
+
+        return yields
+
+    def _setupDirectory(self, home, temp_dir, runid):
+
+        # TODO should be cross platform delimiter
+        temp_run_dir = "%s/dssatrun%04d" % (temp_dir,runid) 
+
+        copytree(home, temp_run_dir)
+
+        
+
+    def _editIrr(self, fileio_in, fileio_out, irrsched):
 
         # Read data 
-        file_name = "DSSAT47.INP"
-        reader = open(file_name, "r")
+        reader = open(fileio_in, "r")
 
         raw_txt = reader.readlines()
 
-        # Constants
-#        dat = np.array([
-#                [2017150,1.0],
-#                [2017161,3.22],
-#                [2017171,14.1],
-#                [2017181,34.1],
-#                [2017191,3.6],
-#                [2017201,7.4],
-#                [2017211,4.4],
-#                [2017222,31.2],
-#                [2017232,5.7],
-#                [2017242,1.1]
-#            ])
-
-        frmdatarr = np.apply_along_axis(
-                (lambda a : "   %d IR001  %f" % (a[0], a[1])),
-                1,irrsched
-                )
+        frmdatarr = np.apply_along_axis( (lambda a : "   %d IR001  %f" % (a[0], a[1])), 1,irrsched)
 
         frmdat = reduce(
                 lambda a,b : "%s\n\r%s" % (a,b) 
@@ -67,6 +87,19 @@ class dssat4dum():
                 irrigation_it = -1
 
 
-        print(raw_result)
+        return raw_result
+
+        
+dssat_home = "../../rundir/"
+fileio = "../../rundir/DSSAT47.INP"
+tmp_dir = "/tmp/"
+
+runner = Dssat4Dum(dssat_home, fileio, tmp_dir)
+
+irrsched = np.array(np.matrix('[2017123, 22; 2017140, 4]'))
+
+print(runner.run(irrsched))
+
+
 
 
