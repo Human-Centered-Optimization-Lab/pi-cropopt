4ab3132cb0cbae90d548e46f4080e8cc4e6cf66c
Ian Kropp <kroppian@msu.edu>
2020-06-24 12:47:41 -0400

    Problem initialization

--- src/python/cropopt.py
+++ src/python/cropopt.py
@@ -4,33 +4,55 @@ from multiprocessing import Pool
 
 class CropOpt(Problem):
 
+    MIN_DATE    = 0
+    MAX_DATE    = 1
+    MIN_APP     = 2
+    MAX_APP     = 3
 
-    def __init__(self, threads, dssat_home, dssat_inp, tmp_dir):
+    #
+    # threads       -- Number of threads for optimization
+    # dssat_home    -- Where all of your dssat files are
+    # tmp_dir       -- Where to set up the run directories 
+    # date_ranges   -- 4xn array of period beginnings, period endings, period 
+    #                   minimums, and period maximums
+    # 
+    def __init__(self, threads, dssat_home, dssat_inp, date_ranges):
 
-        self.threads = 4
-        self.dssat_home = dssat_home
-        self.dssat_inp = dssat_inp
-        self.tmp_dir = tmp_dir
+        self.threads        = threads
+        self.dssat_home     = dssat_home
+        self.dssat_inp      = dssat_inp
+        self.tmp_dir        = tmp_dir
+        self.date_ranges    = date_ranges
 
-        super().__init__(n_var=self.relay_count*2, # we have a lat and long coordinate for each relay
+        # +1 to avoid fencepost error
+        day_count = np.sum(
+                self.date_ranges[:,MAX_DATE] - self.date_ranges[:,MIN_DATE] + 1
+                ) 
+
+        # TODO make sure we don't need any constraints
+        super().__init__(n_var=day_count,
                          n_obj=2,
                          n_constr=0,
-                         xl=self.relay_lower_bounds,
-                         xu=self.relay_upper_bounds)
+                         xl=self.date_ranges[:,MIN_APP],
+                         xu=self.date_ranges[:,MAX_APP])
 
     def _evaluate(self, x, out, *args, **kwargs):
 
-
         d_runner = dssat4py(self.dssat_home, self.dssat_inp, self.tmp_dir)
 
+        # message x to work in eval_batch
 
-        with Pool(processes=self.threads) as pool:
-            objs = pool.map(d_runner.eval_irr, [(variables)])
+        irrsched = self._format_irrigation(x)
 
+        objectives = d_runner.eval_batch(irrsched, self.threads)
 
         out["F"] = objectives
 
 
+    def _format_irrigation(self, x):
 
+        # TODO impelement 
+
+        return x
 
 
