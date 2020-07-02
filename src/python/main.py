40067602e231af383e7ce40bd075c1d16df54667
Ian Kropp <kroppian@msu.edu>
2020-07-02 14:36:35 -0400

    Setting up comparative runs between having and not having cropover

--- src/python/main.py
+++ src/python/main.py
@@ -1,4 +1,4 @@
-from pymoo.algorithms.nsga2 import NSGA2
+from pymoo.algorithms.nsga3 import NSGA3
 from pymoo.optimize import minimize
 from pymoo.visualization.scatter import Scatter
 import numpy as np
@@ -8,13 +8,16 @@ import os
 from numpy import genfromtxt
 from cropover import Cropover
 from pathlib import Path
+from pymoo.factory import get_reference_directions
 
 Configuration.show_compile_hint = False
 
 from cropopt import CropOpt
 
 
-max_run = 50 
+### Parameters ###
+
+max_run = 10
 
 dateTimeObj = datetime.now()
 timestamp = "%d-%02d-%02d_%02d-%02d-%02d" % (dateTimeObj.year, dateTimeObj.month, dateTimeObj.day, dateTimeObj.hour, dateTimeObj.minute, dateTimeObj.second)
@@ -30,76 +33,69 @@ outputdir = "/home/ian/Projects/dssat4py/src/python/output/"
 timestamp = "%d-%02d-%02d_%02d-%02d-%02d" % (dateTimeObj.year, dateTimeObj.month, dateTimeObj.day, dateTimeObj.hour, dateTimeObj.minute, dateTimeObj.second)
 outputdir = outputdir + "batch" + timestamp
 Path(outputdir).mkdir(parents=True, exist_ok=True)
+generations = 200
 
 threads = 4 
 
-date_ranges = np.array(np.matrix("[2017135, 2017140, 1, 20; 2017200, 2017204, 0, 19]"))
+date_ranges = np.array(np.matrix("[2017198, 2017299, 0, 10]"))
+
+ref_dirs = get_reference_directions("energy", 3, 90, seed=1)
+
+### Functions ###
+
+def startRuns(with_co):
+
+    for run in range(max_run):
+
+        if with_co:
+            seed = seeds[run]
+            fileName = "with_run"
+        else:
+            seed = seeds[run + max_run]
+            fileName = "without_run"
 
 
-# Runs with cropover
-for run in range(max_run):
-
-    seed = seeds[run + max_run]
+        gen_dir =  outputdir + "/gendat_%s/" % fileName
+        Path(gen_dir).mkdir(parents=True, exist_ok=True)
 
 
+        prob = CropOpt(threads, dssat_home, fileio, tempdir, 
+                date_ranges, gen_dir, run, seed=seed)
 
-    prob = CropOpt(threads, dssat_home, fileio, tempdir, 
-            date_ranges, outputdir, run, seed=seed)
+        cropover = Cropover(eta=30, prob=1.0)
 
-    cropover = Cropover(eta=30, prob=1.0)
+        if with_co:
+            algorithm = NSGA3(pop_size=100, 
+                    ref_dirs=ref_dirs,
+                    eliminate_duplicates=True,
+                    crossover=cropover)
+        else: 
+            algorithm = NSGA3(pop_size=100, 
+                    ref_dirs=ref_dirs,
+                    eliminate_duplicates=True)
 
-    algorithm = NSGA2(pop_size=100, 
-            eliminate_duplicates=True,
-            #sampling=s_sampler,
-            crossover=cropover)
+        res = minimize(prob,
+                       algorithm,
+                       ('n_gen', generations),
+                       seed=seed,
+                       verbose=True)
 
-    res = minimize(prob,
-                   algorithm,
-                   ('n_gen', 200),
-                   seed=seed,
-                   verbose=True)
+        paretoFront = res.F
+        paretoFront[:,1] = paretoFront[:,1]*-1
 
-    print("Optima with regards to yield: %d" % prob.get_optimal())
+        np.savetxt("%s/%s%04d.csv" % (outputdir, fileName, run), paretoFront, delimiter=",")
 
-    paretoFront = res.F
-    paretoFront[:,1] = paretoFront[:,1]*-1
-    
-    dir_name = "results/%s" % timestamp
-
-    if not os.path.exists(dir_name):
-        os.mkdir(dir_name)
-
-    np.savetxt("%s/with_run%04d.csv" % (dir_name,run), paretoFront, delimiter=",")
+        print("\n\n========== Run %d complete ==========\n\n" % run)
 
 
-# Runs without cropover
-for run in range(max_run):
+### Main ###
 
-    seed = seeds[run]
+print("\n\n========== With CropOpt run starting ==========\n\n" )
+startRuns(True)
+print("\n\n========== With CropOpt run complete ==========\n\n" )
+startRuns(False)
+print("\n\n========== Without CropOpt run complete ==========\n\n")
 
-    prob = Cropover(seed=seed)
-    cropover = Cropover(eta=30, prob=1.0)
-
-    algorithm = NSGA2(pop_size=100, 
-            eliminate_duplicates=True)#,
-            #sampling=s_sampler)
-
-    res = minimize(prob,
-                   algorithm,
-                   ('n_gen', 200),
-                   seed=seed,
-                   verbose=True)
-
-    print("Optima with regards to yield: %d" % prob.get_optimal())
-
-    paretoFront = res.F
-    paretoFront[:,1] = paretoFront[:,1]*-1
-    
-    dir_name = "results/%s" % timestamp
-    if not os.path.exists(dir_name):
-        os.mkdir(dir_name)
-
-    np.savetxt("%s/without_run%04d.csv" % (dir_name,run), paretoFront, delimiter=",")
 
 
 
