a142fdf492917f2a2e28ef9d63ffb489e482def1
Ian Kropp <iankropp@gmail.com>
2020-07-21 14:50:12 -0400

    Tidying up the app counter code a bit

--- src/python/appCounter.py
+++ src/python/appCounter.py
@@ -1,36 +1,45 @@
 import numpy as np
 from plotHV import get_nondoms
 
-#dat_file = "/Users/iankropp/tempMount/run4/gendat_with_run/run0000_gen0199_var.csv"
-var_file = "/Users/iankropp/tempMount/run4/batch2020-07-16_16-23-26/gendat_with_run/run0000_gen0199_var.csv"
-obj_file = "/Users/iankropp/tempMount/run4/batch2020-07-16_16-23-26/gendat_with_run/run0000_gen0199_obj.csv"
+def app_counter(var_file, obj_file, vars2exclude=[], objs2exclude=[]):
 
-varz = np.genfromtxt(var_file, delimiter=',')
-obj  = np.genfromtxt(obj_file, delimiter=',')
+    varz = np.genfromtxt(var_file, delimiter=',')
+    obj  = np.genfromtxt(obj_file, delimiter=',')
 
-# Ignore the middle column, because it's all zeros 
-varz = varz[get_nondoms(obj[:,(0,2)]),:]
+    objs_to_include = set(range(np.size(obj, 1)))
 
-cols2exclude = [102]
+    for obj2exclude in objs2exclude:
+        objs_to_include.remove(obj2exclude)
 
-col_count = np.size(varz,1)
+    # Ignore the middle column, because it's all zeros 
+    varz = varz[get_nondoms(obj[:,list(objs_to_include)]),:]
 
-cols = set(range(col_count))
+    col_count = np.size(varz,1)
 
-for col2ex in cols2exclude: 
-    cols.remove(col2ex)
+    cols = set(range(col_count))
 
-varz = varz[:,list(cols)]
+    for col2ex in vars2exclude: 
+        cols.remove(col2ex)
 
-varz[varz != 0] = 1
+    varz = varz[:,list(cols)]
 
-totals_ones = np.sum(varz,1)    
+    varz[varz != 0] = 1
 
-print(totals_ones)
-print(np.size(totals_ones))
+    totals_ones = np.sum(varz,1)    
 
-b = 0
+    return totals_ones
 
-#totals_zeros =  
+
+if __name__ == '__main__':
+
+    var_file = "/Users/iankropp/tempMount/run4/batch2020-07-16_16-23-26/gendat_with_run/run0000_gen0199_var.csv"
+    obj_file = "/Users/iankropp/tempMount/run4/batch2020-07-16_16-23-26/gendat_with_run/run0000_gen0199_obj.csv"
+
+    o2exclude = [1]
+
+    v2exclude = [102]
+
+    print(app_counter(var_file, obj_file, v2exclude, o2exclude))
+
 
 
