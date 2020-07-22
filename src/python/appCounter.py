1869b6732ac56fb952ff69a09a950fb24470efc2
Ian Kropp <iankropp@gmail.com>
2020-07-22 08:59:09 -0400

    Removing the file processing out of the app counter

--- src/python/appCounter.py
+++ src/python/appCounter.py
@@ -1,10 +1,7 @@
 import numpy as np
 from plotHV import get_nondoms
 
-def app_counter(var_file, obj_file, vars2exclude=[], objs2exclude=[]):
-
-    varz = np.genfromtxt(var_file, delimiter=',')
-    obj  = np.genfromtxt(obj_file, delimiter=',')
+def app_counter(varz, obj, vars2exclude=[], objs2exclude=[]):
 
     objs_to_include = set(range(np.size(obj, 1)))
 
@@ -35,11 +32,14 @@ if __name__ == '__main__':
     var_file = "/Users/iankropp/tempMount/run4/batch2020-07-16_16-23-26/gendat_with_run/run0000_gen0199_var.csv"
     obj_file = "/Users/iankropp/tempMount/run4/batch2020-07-16_16-23-26/gendat_with_run/run0000_gen0199_obj.csv"
 
+    varz = np.genfromtxt(var_file, delimiter=',')
+    obj  = np.genfromtxt(obj_file, delimiter=',')
+
     o2exclude = [1]
 
     v2exclude = [102]
 
-    print(app_counter(var_file, obj_file, v2exclude, o2exclude))
+    print(app_counter(varz, obj, v2exclude, o2exclude))
 
 
 
