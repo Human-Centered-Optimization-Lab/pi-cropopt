88b20d42077588402966e3c301c8bcc1713ef25e
Ian Kropp <iankropp@gmail.com>
2021-04-01 11:10:02 -0400

    Have the cropopt date ranges working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 ## Parameters: 
 
@@ -10,6 +11,7 @@ reps = 20
 year = 2000
 plant_date = 135 
 
+total_nitro = 200
 
 
 ## Derived parameters 
@@ -21,12 +23,39 @@ first_nut_app = plant_date
 
 # Calcualte irrigation bounds
 # (between the 30-year minimum of V6 and maximum of R2)
-irr_lb = min(app_man[app_man.Year < 2010].V8)  # TODO change to V6 when data is available
-irr_up = max(app_man[app_man.Year < 2010].R2)
+irr_date_lb = min(app_man[app_man.Year < 2010].V8)  # TODO change to V6 when data is available
+irr_date_ub = max(app_man[app_man.Year < 2010].R2)
 
-nitro_lb = min(app_man[app_man.Year < 2010].V8)  # TODO change to V6 when data is available
-nitro_up = max(app_man[app_man.Year < 2010].V14)
+nitro_date_lb = min(app_man[app_man.Year < 2010].V8)  # TODO change to V6 when data is available
+nitro_date_ub = max(app_man[app_man.Year < 2010].V14)
 
+# Reformat the mins and maxes for the given year
+# 
+irr_date_lb += int(year * 1e3)
+irr_date_ub += int(year * 1e3)
+nitro_date_lb += int(year * 1e3)
+nitro_date_ub += int(year * 1e3)
+plant_date += int(year * 1e3)
+
+#
+# Irrigation type            Nutrient type
+# Col 1: Period begin date   Col 1: Period begin date  
+# Col 2: Period end date     Col 2: Period end date    
+# Col 3: IRR=0,              Col 3: NUT=1,             
+# Col 4: IRR min             Col 4: Nitrogen amount    
+# Col 5: IRR max             Col 5: Phos amount        
+# Col 6: 0                   Col 6: Pot amount         
+#
+
+date_ranges = [
+        [irr_date_lb,   irr_date_ub,    0,                     0, 10, 0],       # Irrigation period 
+        [plant_date,    plant_date,     1, int(total_nitro*0.75),  0, 0],  # Preplant incorporation 
+        [nitro_date_lb, nitro_date_ub,  1, int(total_nitro*0.25),  0, 0]]   
+
+
+date_ranges = np.array(date_ranges)
+
+print(date_ranges)
 
 # 1: Year
 # 2: Irrigation period 
