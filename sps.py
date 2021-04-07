1aa5750d160efd2ac1a849de609fc4cf0a86b671
Ian Kropp <iankropp@gmail.com>
2021-04-07 14:59:22 -0400

    Updating the parameter to SPS to be sparsity

--- sps.py
+++ sps.py
@@ -2,10 +2,10 @@ from pymoo.model.sampling import Sampling
 import numpy as np
 import random
 
-class SparseSampler(Sampling):
+class SPS(Sampling):
 
-    def __init__(self, n, sampled_mask=[]):
-        self.n = n
+    def __init__(self, sparsity, sampled_mask=[]):
+        self.sparsity = sparsity
         self.sampled_mask = sampled_mask
 
     def _do(self, problem, n_samples, **kwargs):
@@ -15,8 +15,8 @@ class SparseSampler(Sampling):
         for row in range(np.size(X,0)):
 
             # Where to put the samples 
-            #indices = np.random.randint(0,  np.shape(X)[1], self.n)
-            indices = random.sample(range(np.shape(X)[1]), self.n)
+            genome_size = np.shape(X)[1]
+            indices = random.sample(range(genome_size), round(genome_size*self.sparsity))
 
             # Include any indices that alwasy need to be non-zero
             indices = list(set(indices).union(self.sampled_mask))
