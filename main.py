1d0da66b80904c373ddf39fab6508ae601473cad
Ian Kropp <iankropp@gmail.com>
2021-04-13 16:52:03 -0400

    Made the year a command line parameter

--- main.py
+++ main.py
@@ -20,28 +20,28 @@ if __name__ == "__main__":
     app_man_csv = "management_dates.csv"
     app_man = pd.read_csv(app_man_csv)
 
-    reps = 1
+    reps = 10
     year = 2000
-    plant_date = 135 
+    plant_date = 136
 
     total_nitro = 200
 
     # dssat parameters 
-    home_dir = "/Users/iankropp"
+    home_dir = "/work/ian/"
 
     dssat_home = "%s/Projects/agovization/dhome" % home_dir
-    dssat_exe = "%s/Projects/agovization/dhome/dscsm047" % home_dir
+    dssat_exe = "%s/Projects/agovization/dhome/dscsm047-linux" % home_dir
     dssat_inp = "%s/Projects/agovization/dhome/DSSAT47.INP" % home_dir
     output_dir = "%s/Projects/agovization/output/" % home_dir
 
     tmp_dir = "/tmp/"
 
     # Runtime optimization parameters
-    threads = 6
+    threads = 20
     initial_sparsity = 0.1
     pop_size = 100
     ref_dirs = get_reference_directions("energy", 3, 90, seed=1)
-    generations = 2
+    generations = 200
 
     ## Derived parameters 
 
@@ -133,7 +133,7 @@ if __name__ == "__main__":
             objectives = np.array([indiv.F for indiv in gen.pop ])
             x = np.array([indiv.X for indiv in gen.pop ])
 
-            full_output_dir = "%s/batch%s" % (output_dir, timestamp)
+            full_output_dir = "%s/year_%s_%s" % (output_dir, str(year), timestamp)
 
             Path(full_output_dir).mkdir(parents=True, exist_ok=True)
 
