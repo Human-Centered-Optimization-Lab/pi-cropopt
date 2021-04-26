9b6e3515543f12b55a7eb8f10539b696ee15e3bd
Ian Kropp <iankropp@gmail.com>
2021-04-26 14:01:16 -0400

    Making the main parameter a command line parameter

--- show_table.py
+++ show_table.py
@@ -1,8 +1,10 @@
 import pickle
 import tabloo
+import sys
 
+file_name = sys.argv[1]
 
-infile = open('step1_optAndPesFronts.pkl', 'rb')
+infile = open(file_name, 'rb')
 candid_tab = pickle.load(infile)
 
 tabloo.show(candid_tab)
