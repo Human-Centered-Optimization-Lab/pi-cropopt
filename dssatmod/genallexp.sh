511d003694a7c40e3a880f74739edad7b690cf95
Ian Kropp <iankropp@gmail.com>
2021-05-06 10:59:35 -0400

    Adding a little helper script to run genexp for all years

---
+++ dssatmod/genallexp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+python  dssatmod/genexp.py 1980 dhome/CASSTEMPLATE.MZX > dhome/CASS8001.MZX
+python  dssatmod/genexp.py 1981 dhome/CASSTEMPLATE.MZX > dhome/CASS8101.MZX
+python  dssatmod/genexp.py 1982 dhome/CASSTEMPLATE.MZX > dhome/CASS8201.MZX
+python  dssatmod/genexp.py 1983 dhome/CASSTEMPLATE.MZX > dhome/CASS8301.MZX
+python  dssatmod/genexp.py 1984 dhome/CASSTEMPLATE.MZX > dhome/CASS8401.MZX
+python  dssatmod/genexp.py 1985 dhome/CASSTEMPLATE.MZX > dhome/CASS8501.MZX
+python  dssatmod/genexp.py 1986 dhome/CASSTEMPLATE.MZX > dhome/CASS8601.MZX
+python  dssatmod/genexp.py 1987 dhome/CASSTEMPLATE.MZX > dhome/CASS8701.MZX
+python  dssatmod/genexp.py 1988 dhome/CASSTEMPLATE.MZX > dhome/CASS8801.MZX
+python  dssatmod/genexp.py 1989 dhome/CASSTEMPLATE.MZX > dhome/CASS8901.MZX
+python  dssatmod/genexp.py 1990 dhome/CASSTEMPLATE.MZX > dhome/CASS9001.MZX
+python  dssatmod/genexp.py 1991 dhome/CASSTEMPLATE.MZX > dhome/CASS9101.MZX
+python  dssatmod/genexp.py 1992 dhome/CASSTEMPLATE.MZX > dhome/CASS9201.MZX
+python  dssatmod/genexp.py 1993 dhome/CASSTEMPLATE.MZX > dhome/CASS9301.MZX
+python  dssatmod/genexp.py 1994 dhome/CASSTEMPLATE.MZX > dhome/CASS9401.MZX
+python  dssatmod/genexp.py 1995 dhome/CASSTEMPLATE.MZX > dhome/CASS9501.MZX
+python  dssatmod/genexp.py 1996 dhome/CASSTEMPLATE.MZX > dhome/CASS9601.MZX
+python  dssatmod/genexp.py 1997 dhome/CASSTEMPLATE.MZX > dhome/CASS9701.MZX
+python  dssatmod/genexp.py 1998 dhome/CASSTEMPLATE.MZX > dhome/CASS9801.MZX
+python  dssatmod/genexp.py 1999 dhome/CASSTEMPLATE.MZX > dhome/CASS9901.MZX
+python  dssatmod/genexp.py 2000 dhome/CASSTEMPLATE.MZX > dhome/CASS0001.MZX
+python  dssatmod/genexp.py 2001 dhome/CASSTEMPLATE.MZX > dhome/CASS0101.MZX
+python  dssatmod/genexp.py 2002 dhome/CASSTEMPLATE.MZX > dhome/CASS0201.MZX
+python  dssatmod/genexp.py 2003 dhome/CASSTEMPLATE.MZX > dhome/CASS0301.MZX
+python  dssatmod/genexp.py 2004 dhome/CASSTEMPLATE.MZX > dhome/CASS0401.MZX
+python  dssatmod/genexp.py 2005 dhome/CASSTEMPLATE.MZX > dhome/CASS0501.MZX
+python  dssatmod/genexp.py 2006 dhome/CASSTEMPLATE.MZX > dhome/CASS0601.MZX
+python  dssatmod/genexp.py 2007 dhome/CASSTEMPLATE.MZX > dhome/CASS0701.MZX
+python  dssatmod/genexp.py 2008 dhome/CASSTEMPLATE.MZX > dhome/CASS0801.MZX
+python  dssatmod/genexp.py 2009 dhome/CASSTEMPLATE.MZX > dhome/CASS0901.MZX
+python  dssatmod/genexp.py 2010 dhome/CASSTEMPLATE.MZX > dhome/CASS1001.MZX
+
