db3a5840c3a595f61ec3d1b1e4173d5600ea1fe3
Ian Kropp <kroppian@msu.edu>
2021-09-13 13:48:15 -0400

    Removing values with zero variance

--- mlearn/step1_machine_learning.R
+++ mlearn/step1_machine_learning.R
@@ -1,5 +1,7 @@
 #install.packages("arrow")
+#install.packages("caret")
 
+library(caret)
 library(feather);
 
 stage2num <- function(stage){ 
@@ -11,14 +13,14 @@ path = "Z:\\Gilgamesh\\kroppian\\agovization_results\\mlearning\\2021-09-10_13-3
 
 stages <- c("P","V6",  "V7", "V8", "V9", "V10",  "V11", "V12", "V13", "V14", "R1", "R2", "R3", "R4")
 
-#raw_attribute_tab <- arrow::read_feather(path)
-#attribute_tab <- subset(raw_attribute_tab, select=-growth_period_of_second_N_app)
-#attribute_tab['growth_period_of_second_N_app'] <- apply(raw_attribute_tab['growth_period_of_second_N_app'], MARGIN=1, stage2num)
-
-
-
+# Read data
 attribute_tab <- arrow::read_feather(path)
+
+# Clean data and make it all numeric
 attribute_tab['growth_period_of_second_N_app'] <- apply(attribute_tab['growth_period_of_second_N_app'], MARGIN=1, stage2num)
 
+# Exclude columns with zero variance (all the same values)
+# nearZeroVar(attribute_tab, saveMetrics = TRUE) for more granular results
+zeroVars = nearZeroVar(attribute_tab, freqCut = 10000)
 
-
+attribute_tab <- attribute_tab[, -zeroVars]
