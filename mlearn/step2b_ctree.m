f143b54a8584fa309b042cb3faf7edd6aee615e4
Ian Kropp <iankropp@gmail.com>
2021-09-21 10:24:05 -0400

    Cleaning up for macbook

--- mlearn/step2b_ctree.m
+++ mlearn/step2b_ctree.m
@@ -1,17 +1,20 @@
 
 
 %% Constants
-
+% FEATURE_TABLE_PATH = "Z:\Gilgamesh\kroppian\agovization_results\mlearning\features.xlsx";
+% TARGET_TABLE_PATH = "Z:\Gilgamesh\kroppian\agovization_results\mlearning\targets.xlsx";
+FEATURE_TABLE_PATH = "/Volumes/data/Gilgamesh/kroppian/agovization_results/mlearning/features.xlsx";
+TARGET_TABLE_PATH = "/Volumes/data/Gilgamesh/kroppian/agovization_results/mlearning/targets.xlsx";
 
 
 %% Load data
 
 if ~exist("features", 'var')
-    features = readtable("Z:\Gilgamesh\kroppian\agovization_results\mlearning\features.xlsx");
+    features = readtable(FEATURE_TABLE_PATH);
 end
 
 if ~exist("targets_raw", 'var')
-    targets_raw = readtable("Z:\Gilgamesh\kroppian\agovization_results\mlearning\targets.xlsx");
+    targets_raw = readtable(TARGET_TABLE_PATH);
 end
 targets = targets_raw;
 
@@ -32,30 +35,19 @@ targets.leaching = targets_raw.leaching < 10;
 
 
 %% fit the tree for front
-% For the optimization and plots, the objective function is 
-% log(1 + cross-validation loss) for regression and the misclassification 
-% rate for classification.
 
-% ------------------
-% params = hyperparameters('fitctree',features, targets.front);
-% params(1).Range = [1,20];
-% 
-% res = fitctree(features, targets.front, 'OptimizeHyperparameters',params );
-% 
-% trainingOptions = res.ModelParameters;
-% ------------------
+%% front
+%mdlFront = fitctree(features, targets.front, 'MaxNumSplits', 20, 'CrossVal', 'on');
 
-% front
-%mdl20 = fitctree(features, targets.front, 'MaxNumSplits', 20, 'CrossVal', 'on');
+%% yield
+mdlYield = fitctree(features, targets.yield, 'MaxNumSplits', 15, 'CrossVal', 'on');
+view(mdlYield.Trained{1},'Mode','graph')
 
-% yield
-mdl20 = fitctree(features, targets.yield, 'MaxNumSplits', 15, 'CrossVal', 'on');
-view(mdl20.Trained{1},'Mode','graph')
+
 
 % leaching
-
-mdl20 = fitctree(features, targets.leaching, 'MaxNumSplits', 10, 'CrossVal', 'on');
-view(mdl20.Trained{1},'Mode','graph')
+% mdlLeaching = fitctree(features, targets.leaching, 'MaxNumSplits', 10, 'CrossVal', 'on');
+% view(mdlLeaching.Trained{1},'Mode','graph')
 
 
 
