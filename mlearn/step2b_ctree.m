cbc3194d208762f9f3d7687d116dfc50a276dce5
Ian Kropp <iankropp@gmail.com>
2021-09-21 13:53:17 -0400

    Have yield tree learning working quite well

--- mlearn/step2b_ctree.m
+++ mlearn/step2b_ctree.m
@@ -17,34 +17,69 @@ if ~exist("targets_raw", 'var')
     targets_raw = readtable(TARGET_TABLE_PATH);
 end
 targets = targets_raw;
+total_samples = size(targets, 1);
 
-%% Make front a boolean
+
+
+%% Booleanize (booleanaise?)
+
+% Make front a boolean
 targets.front = targets_raw.front == 0;
 cp = classperf(targets.front);
 
-%% Make yield a boolean
+% Make yield a boolean
 
 sortedYield = sort(targets_raw.yield_);
 upperQuartile = sortedYield(uint32(length(sortedYield)*.75));
 targets.yield = targets_raw.yield_ > upperQuartile;
 
-%% Make leaching a boolean
+% Make leaching a boolean
 sortedLeaching = sort(targets_raw.leaching);
 lowerQuartile = sortedLeaching(uint32(length(sortedLeaching)*.25));
 targets.leaching = targets_raw.leaching < 10;
 
+%% split data into training and testing subsets
 
-%% fit the tree for front
+% Figure the number of training and testing samples
+train_ind = zeros(total_samples,1);
+total_training_samples = uint32(total_samples*0.8);
+total_testing_samples = total_samples - total_training_samples;
 
-%% front
+% Randomly determine location of training indices
+train_ind(randperm(total_samples, total_training_samples), 1) = 1;
+test_ind = ~train_ind;
+
+train_ind = find(train_ind);
+test_ind = find(test_ind);
+
+% Report on preportions of fronts 
+fprintf("Total proportion of yield: %f\n", sum(targets.yield == 1)/total_samples);
+
+fprintf("Test proportion of yield: %f\n", sum(targets(test_ind,:).yield == 1)/double(total_testing_samples));
+
+fprintf("Training proportion of yield: %f\n", sum(targets(train_ind,:).yield == 1)/double(total_training_samples));
+
+
+%% fit the tree for various objectives
+
+% front
 %mdlFront = fitctree(features, targets.front, 'MaxNumSplits', 20, 'CrossVal', 'on');
 
-%% yield
-mdlYield = fitctree(features, targets.yield, 'MaxNumSplits', 15);
+% yield
+mdlYield = fitctree(features(train_ind,:), targets(train_ind,:).yield, 'MaxNumSplits', 15);
 view(mdlYield,'Mode','graph')
-yHat = predict(mdlYield, features);
 
-trainingError = sum(yHat == targets.front)/length(targets.front);
+yHatTest = predict(mdlYield, features(test_ind,:));
+testAccur = sum(yHatTest == targets(test_ind,:).yield)/double(total_testing_samples);
+
+yHatTrain = predict(mdlYield, features(train_ind,:));
+trainingAccur = sum(yHatTrain == targets(train_ind,:).yield)/double(total_training_samples);
+
+
+fprintf("Training error: %f\n", trainingAccur);
+fprintf("Test error: %f\n", testAccur);
+
+
 
 % leaching
 % mdlLeaching = fitctree(features, targets.leaching, 'MaxNumSplits', 10, 'CrossVal', 'on');
