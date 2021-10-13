ae9e5f5315371d6fbc0baa94808fd072719c5930
Ian Kropp <kroppian@msu.edu>
2021-10-13 16:48:44 -0400

    Removing more features, and renaming the features for ease of reading

--- mlearn/cleanUpFeatures.m
+++ mlearn/cleanUpFeatures.m
@@ -4,6 +4,7 @@ function [editedFeatures] = cleanUpFeatures(features)
 
 editedFeatures = features;
 
+%% Make compound variables
 editedFeatures.total_wat_during_v6 = features.total_irr_during_v6 + features.total_precip_during_v6;
 editedFeatures.total_wat_during_v7 = features.total_irr_during_v7 + features.total_precip_during_v7;
 editedFeatures.total_wat_during_v8 = features.total_irr_during_v8 + features.total_precip_during_v8;
@@ -18,6 +19,7 @@ editedFeatures.total_wat_during_R2 = features.total_irr_during_R2 + features.tot
 editedFeatures.total_wat_during_R3 = features.total_irr_during_R3 + features.total_precip_during_R3;
 editedFeatures.total_wat_during_R4 = features.total_irr_during_R4 + features.total_precip_during_R4;
 
+%% Clear out unhelpful variables
 editedFeatures.freq_precip_during_v8   = [];
 editedFeatures.freq_precip_during_v9   = [];
 editedFeatures.freq_precip_during_v10  = [];
@@ -72,7 +74,19 @@ editedFeatures.total_precip_during_R2   = [];
 editedFeatures.total_precip_during_R3   = []; 
 editedFeatures.total_precip_during_R4   = []; 
 
+
 editedFeatures.number_of_precipitation_events = [];
+editedFeatures.application_count = [];
+editedFeatures.total_irrigation = [];
+
+editedFeatures.minimum_irr = [];
+editedFeatures.maximum_irr = [];
+
+%% rename 
+editedFeatures.Properties.VariableNames = {'period_2_N_app', 'climate', ...
+    'total_p', 'tot_wat_v6', 'tot_wat_v7', 'tot_wat_v8', 'tot_wat_v9', ...
+    'tot_wat_v10', 'tot_wat_v11', 'tot_wat_v12', 'tot_wat_v13', ...
+    'tot_wat_v14', 'tot_wat_R1', 'tot_wat_R2', 'tot_wat_R3', 'tot_wat_R4'};
 
 
 
