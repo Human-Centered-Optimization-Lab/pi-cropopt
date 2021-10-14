a518dd75a264c602717de12f7ca4fa893d9d10ab
Ian Kropp <iankropp@gmail.com>
2021-10-14 16:33:39 -0400

    Normalized yield by year, which has resulted in a very nice gaussian

--- mlearn/step2e_stat_tests.R
+++ mlearn/step2e_stat_tests.R
@@ -4,33 +4,41 @@ ATTRIB_TAB <- "/Volumes/data/Gilgamesh/kroppian/agovization_results/mlearning/at
 
 attrib_tab <- arrow::read_feather(ATTRIB_TAB)
 
+## Normalize yield across all years
+max_yields <- attrib_tab %>%
+              group_by(year) %>%
+              summarize(year_max_yield = max(yield_)) 
+
+norm_yield <- attrib_tab %>%
+              inner_join(max_yields) %>%
+              select(yield_, year_max_yield) %>%
+              mutate(norm_yield = yield_ / year_max_yield) %>%
+              pull(norm_yield)
+       
+attrib_tab$norm_yield <- norm_yield
+
+
 ## find high yielding solutions
 
 # Get the threshold for high yields
-yields <- attrib_tab$yield_
-yield_count <- length(yields)
-sorted_yields <- sort(yields)
-quartile_locale <- round(yield_count * 0.75)
-quartile_threshold <- sorted_yields[quartile_locale]
   
-high_yielders <- filter(attrib_tab, yield_ > quartile_threshold) %>%
-                 filter(front == 0) %>%
-                 filter(climate == 2) 
+high_yielders <- filter(attrib_tab, norm_yield > 0.75) 
 
 #high_yielders <- filter(attrib_tab, front == 0)
 
 dev.off(dev.list()["RStudioGD"]) # Clears plots
-histogram(high_yielders$total_wat_during_v6,  nint=30)
-histogram(high_yielders$total_wat_during_v7,  nint=30)
-histogram(high_yielders$total_wat_during_v8,  nint=30)
-histogram(high_yielders$total_wat_during_v9,  nint=30)
-histogram(high_yielders$total_wat_during_v10, nint=30)
-histogram(high_yielders$total_wat_during_v11, nint=30)
-histogram(high_yielders$total_wat_during_v12, nint=30)
-histogram(high_yielders$total_wat_during_v13, nint=30)
-histogram(high_yielders$total_wat_during_v14, nint=30)
-histogram(high_yielders$total_wat_during_R1,  nint=30)
-histogram(high_yielders$total_wat_during_R2,  nint=30)
-histogram(high_yielders$total_wat_during_R3,  nint=30)
-histogram(high_yielders$total_wat_during_R4,  nint=30)
+histogram(high_yielders$total_wat_during_v6,  nint=60)
+histogram(high_yielders$total_wat_during_v7,  nint=60)
+histogram(high_yielders$total_wat_during_v8,  nint=60)
+histogram(high_yielders$total_wat_during_v9,  nint=60)
+histogram(high_yielders$total_wat_during_v10, nint=60)
+histogram(high_yielders$total_wat_during_v11, nint=60)
+histogram(high_yielders$total_wat_during_v12, nint=60)
+histogram(high_yielders$total_wat_during_v13, nint=60)
+histogram(high_yielders$total_wat_during_v14, nint=60)
+histogram(high_yielders$total_wat_during_R1,  nint=60)
+histogram(high_yielders$total_wat_during_R2,  nint=60)
+histogram(high_yielders$total_wat_during_R3,  nint=60)
+histogram(high_yielders$total_wat_during_R4,  nint=60)
+
 
