d0d84c6f17c0e541d2e4b4d6796af1e034907263
Ian Kropp <iankropp@gmail.com>
2021-10-14 20:23:59 -0400

    Making the histogram routine a bit more sophisticated

--- mlearn/step2e_stat_tests.R
+++ mlearn/step2e_stat_tests.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(fitdistrplus)
 
 ATTRIB_TAB <- "/Volumes/data/Gilgamesh/kroppian/agovization_results/mlearning/attrib_tab.feather"
 
@@ -22,23 +23,36 @@ attrib_tab$norm_yield <- norm_yield
 
 # Get the threshold for high yields
   
-high_yielders <- filter(attrib_tab, norm_yield > 0.75) 
+high_yielders <- filter(attrib_tab, norm_yield > 0.75) %>%
+                 filter(front == 0)
 
 #high_yielders <- filter(attrib_tab, front == 0)
 
-dev.off(dev.list()["RStudioGD"]) # Clears plots
-histogram(high_yielders$total_wat_during_v6,  nint=60)
-histogram(high_yielders$total_wat_during_v7,  nint=60)
-histogram(high_yielders$total_wat_during_v8,  nint=60)
-histogram(high_yielders$total_wat_during_v9,  nint=60)
-histogram(high_yielders$total_wat_during_v10, nint=60)
-histogram(high_yielders$total_wat_during_v11, nint=60)
-histogram(high_yielders$total_wat_during_v12, nint=60)
-histogram(high_yielders$total_wat_during_v13, nint=60)
-histogram(high_yielders$total_wat_during_v14, nint=60)
-histogram(high_yielders$total_wat_during_R1,  nint=60)
-histogram(high_yielders$total_wat_during_R2,  nint=60)
-histogram(high_yielders$total_wat_during_R3,  nint=60)
-histogram(high_yielders$total_wat_during_R4,  nint=60)
 
+growth_stages <- c('total_wat_during_v6', 
+                   'total_wat_during_v7', 
+                   'total_wat_during_v8', 
+                   'total_wat_during_v9',
+                   'total_wat_during_v10', 
+                   'total_wat_during_v11',
+                   'total_wat_during_v12',
+                   'total_wat_during_v13',
+                   'total_wat_during_v14',
+                   'total_wat_during_R1', 
+                   'total_wat_during_R2',
+                   'total_wat_during_R3',
+                   'total_wat_during_R4')
+
+
+dev.off(dev.list()["RStudioGD"]) # Clears plots
+
+for(s in 1:length(growth_stages)){
+  stage <- growth_stages[s]
+  data <- high_yielders[,stage] %>% pull(stage) 
+
+  print(min(data))
+  
+  hist(data, main=stage, xlab="Total irrigation")
+  
+}
 
