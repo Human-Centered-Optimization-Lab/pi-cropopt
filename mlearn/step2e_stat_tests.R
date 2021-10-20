ac862534b46f708117d6920f7c5887382125c39a
Ian Kropp <kroppian@msu.edu>
2021-10-20 12:08:55 -0400

    Have the recommended practices and optimized practices working

--- mlearn/step2e_stat_tests.R
+++ mlearn/step2e_stat_tests.R
@@ -1,9 +1,14 @@
 library(dplyr)
 library(fitdistrplus)
+library(R.utils)
+library(stringr)
 
-ATTRIB_TAB <- "/Volumes/data/Gilgamesh/kroppian/agovization_results/mlearning/attrib_tab.feather"
+#ATTRIB_TAB <- "/Volumes/data/Gilgamesh/kroppian/agovization_results/mlearning/attrib_tab.feather"
+ATTRIB_TAB <- "Z:/Gilgamesh/kroppian/agovization_results/mlearning/attrib_tab.feather"
+RECD_PRACTICES_PATH <- "Z:/Gilgamesh/kroppian/agovization_results/mlearning/recommended_practices.feather"
 
 attrib_tab <- arrow::read_feather(ATTRIB_TAB)
+recd_practs <- arrow::read_feather(RECD_PRACTICES_PATH)
 
 ## Normalize yield across all years
 max_yields <- attrib_tab %>%
@@ -12,7 +17,7 @@ max_yields <- attrib_tab %>%
 
 norm_yield <- attrib_tab %>%
               inner_join(max_yields) %>%
-              select(yield_, year_max_yield) %>%
+              dplyr::select(yield_, year_max_yield) %>%
               mutate(norm_yield = yield_ / year_max_yield) %>%
               pull(norm_yield)
        
@@ -44,15 +49,25 @@ growth_stages <- c('total_wat_during_v6',
                    'total_wat_during_R4')
 
 
+
+
 dev.off(dev.list()["RStudioGD"]) # Clears plots
 
 for(s in 1:length(growth_stages)){
-  stage <- growth_stages[s]
-  data <- high_yielders[,stage] %>% pull(stage) 
+  stage_opt <- growth_stages[s]
+  stage_rec <- str_replace(stage_opt, "total_wat_during_", "") %>% capitalize()
+  
+  optimized_irr <- high_yielders[,stage_opt] %>% pull(stage_opt) 
+  
+  recommended_irr <- recd_practs %>% filter(stages == stage_rec) %>% pull(irr)
 
-  print(min(data))
-  
-  hist(data, main=stage, xlab="Total irrigation")
-  
+  all_obvs <- append(optimized_irr,recommended_irr) 
+   
+  b <-  seq(min(all_obvs), max(all_obvs), length.out=15)
+   
+  opt_p <- hist(optimized_irr, col=rgb(0,0,1,1/4), breaks=b)
+  rec_p <- hist(recommended_irr,col=rgb(1,0,0,1/4), breaks=b, add=T) 
+ 
+   
 }
 
