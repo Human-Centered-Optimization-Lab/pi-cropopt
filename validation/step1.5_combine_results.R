e10703529063dc835c583c7d11828280af5a9992
Ian Kropp <iankropp@gmail.com>
2021-12-01 15:15:27 -0500

    Combining the weather station data

--- validation/step1.5_combine_results.R
+++ validation/step1.5_combine_results.R
@@ -1,12 +1,12 @@
-
+library(dplyr)
 
 ROOT_PATH <- "Z:/Gilgamesh/kroppian/agovization_results/validation/"
 ROOT_PATH <- "/Volumes/data/Gilgamesh/kroppian/agovization_results/validation/"
 
-COMM_TABS_OUTPUT    <- paste(ROOT_PATH, "comm_pract_full.feather")
-NITRO_TABS_OUTPUT   <- paste(ROOT_PATH, "nitro_full.feather")
-ALL_RECS_TABS_OUTPUT<- paste(ROOT_PATH, "all_recs_full.feather")
-IRR_TABS_OUTPUT     <- paste(ROOT_PATH, "irr_full.feather")
+COMM_TABS_OUTPUT    <- paste(ROOT_PATH, "comm_pract_full.feather", sep="")
+NITRO_TABS_OUTPUT   <- paste(ROOT_PATH, "nitro_full.feather", sep="")
+ALL_RECS_TABS_OUTPUT<- paste(ROOT_PATH, "all_recs_full.feather", sep="")
+IRR_TABS_OUTPUT     <- paste(ROOT_PATH, "irr_full.feather", sep="")
 
 comm_tabs <- bind_rows(list(
     arrow::read_feather(paste(ROOT_PATH, "comm_pract_run000.feather", sep="")), 
@@ -23,23 +23,9 @@ comm_tabs <- bind_rows(list(
     arrow::read_feather(paste(ROOT_PATH, "comm_pract_run011.feather", sep="")),
     arrow::read_feather(paste(ROOT_PATH, "comm_pract_run012.feather", sep="")),
     arrow::read_feather(paste(ROOT_PATH, "comm_pract_run013.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run014.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run015.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run016.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run017.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run018.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run019.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run020.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run021.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run022.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run023.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run024.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run025.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run026.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run027.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run028.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run029.feather", sep="")) 
+    arrow::read_feather(paste(ROOT_PATH, "comm_pract_run014.feather", sep=""))
 ), .id="run")
+
 
 nitro_tabs <- bind_rows(list(
     arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run000.feather", sep="")), 
@@ -56,22 +42,7 @@ nitro_tabs <- bind_rows(list(
     arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run011.feather", sep="")),
     arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run012.feather", sep="")),
     arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run013.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run014.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run015.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run016.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run017.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run018.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run019.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run020.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run021.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run022.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run023.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run024.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run025.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run026.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run027.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run028.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run029.feather", sep="")) 
+    arrow::read_feather(paste(ROOT_PATH, "nitro_only_result_run014.feather", sep=""))
 ), .id="run")
 
 irr_tabs <- bind_rows(list(
@@ -89,22 +60,7 @@ irr_tabs <- bind_rows(list(
     arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run011.feather", sep="")),
     arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run012.feather", sep="")),
     arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run013.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run014.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run015.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run016.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run017.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run018.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run019.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run020.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run021.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run022.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run023.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run024.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run025.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run026.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run027.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run028.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run029.feather", sep="")) 
+    arrow::read_feather(paste(ROOT_PATH, "irr_only_result_run014.feather", sep=""))
 ), .id="run")
 
 
@@ -123,22 +79,7 @@ all_recs_tabs <- bind_rows(list(
     arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run011.feather", sep="")),
     arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run012.feather", sep="")),
     arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run013.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run014.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run015.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run016.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run017.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run018.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run019.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run020.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run021.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run022.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run023.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run024.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run025.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run026.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run027.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run028.feather", sep="")),
-    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run029.feather", sep="")) 
+    arrow::read_feather(paste(ROOT_PATH, "all_recs_full_result_run014.feather", sep=""))
 ), .id="run")
 
 
