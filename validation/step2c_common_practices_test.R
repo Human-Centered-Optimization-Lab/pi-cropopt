a7388b60ffccfa73ad8dbd899a67711b4c07b76c
Ian Kropp <iankropp@gmail.com>
2021-12-02 10:57:29 -0500

    Adding more titles

--- validation/step2c_common_practices_test.R
+++ validation/step2c_common_practices_test.R
@@ -39,31 +39,31 @@ nitro_only_tyield <- nitro_only_full %>% group_by(run) %>% summarize(total_yield
 all_recs_tyield <- all_recs_full %>% group_by(run) %>% summarize(total_yield = sum(yield)) %>% pull(total_yield)
 
 # individual plots
-hist(comm_pract_tyield, 20)
-hist(nitro_only_tyield, 20)
-hist(all_recs_tyield, 20)
-hist(irr_only_tyield, 20)
+hist(comm_pract_tyield, 10, main="Total 30-year yields for common practices")
+hist(nitro_only_tyield, 10, main="Total 30-year yields for nitrogen recommendations")
+hist(all_recs_tyield, 10, main="Total 30-year yields for all recommendations")
+hist(irr_only_tyield, 10, main="Total 30-year yields irrigation recommendations")
 
 
 # comm vs irr
 max_l <- max(max(comm_pract_tyield), max(irr_only_tyield))*1.05
 min_l <- min(min(comm_pract_tyield), min(irr_only_tyield))*0.95
-hist(comm_pract_tyield, 20, col=rgb(0,0,1,1/4), xlim=c(min_l, max_l), main="Irrigation only recommendations")  # first histogram
-hist(irr_only_tyield, 20, col=rgb(1,0,0,1/4), xlim=c(min_l, max_l), add=T, main="Irrigation only recommendations")  # second
+hist(comm_pract_tyield, 10, col=rgb(0,0,1,1/4), xlim=c(min_l, max_l), main="Total 30-year yields for irrigation only recommendations")  # first histogram
+hist(irr_only_tyield, 10, col=rgb(1,0,0,1/4), xlim=c(min_l, max_l), add=T, main="Total 30-year yields for irrigation only recommendations")  # second
 #legend(x = "topleft", legend = c("Common Practices", "Irrigation recommendations"), fill= c(rgb(0,0,1,1/4), rgb(1,0,0,1/4)))
 
 # comm vs nitro
 max_l <- max(max(comm_pract_tyield), max(nitro_only_tyield))*1.05
 min_l <- min(min(comm_pract_tyield), min(nitro_only_tyield))*0.95
-hist( comm_pract_tyield, 20, col=rgb(0,0,1,1/4), xlim=c(min_l, max_l), main="Nitrogen only recommendations")  # first histogram
-hist( nitro_only_tyield, 20, col=rgb(1,0,0,1/4), xlim=c(min_l, max_l), add=T, main="Nitrogen only recommendations")  # second
+hist( comm_pract_tyield, 10, col=rgb(0,0,1,1/4), xlim=c(min_l, max_l), main="Total 30-year yields for nitrogen only recommendations")  # first histogram
+hist( nitro_only_tyield, 10, col=rgb(1,0,0,1/4), xlim=c(min_l, max_l), add=T, main="Total 30-year yields for nitrogen only recommendations")  # second
 #legend(x = "topleft", legend = c("Common Practices", "Nitrogen recommendations"), fill= c(rgb(0,0,1,1/4), rgb(1,0,0,1/4)))
 
 # comm vs all recs  
 max_l <- max(max(comm_pract_tyield), max(all_recs_tyield))*1.05
 min_l <- min(min(comm_pract_tyield), min(all_recs_tyield))*0.95
-hist( comm_pract_tyield, 20, col=rgb(0,0,1,1/4), xlim=c(min_l, max_l))  # first histogram
-hist( all_recs_tyield, 20, col=rgb(1,0,0,1/4),   xlim=c(min_l, max_l), add=T)  # second
+hist( comm_pract_tyield, 10, col=rgb(0,0,1,1/4), xlim=c(min_l, max_l), main="Total 30-year yields for all recommendations")  # first histogram
+hist( all_recs_tyield, 10, col=rgb(1,0,0,1/4),   xlim=c(min_l, max_l), add=T, main="Total 30-year yields for all recommendations")  # second
 #legend(x = "topleft", legend = c("Common Practices", "All recommendations"), fill= c(rgb(0,0,1,1/4), rgb(1,0,0,1/4)))
 
 
