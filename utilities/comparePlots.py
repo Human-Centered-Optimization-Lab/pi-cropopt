48068ae45ab570392f75df94e9a01df82f498a69
Ian Kropp <iankropp@gmail.com>
2024-06-21 13:07:36 -0400

    Making this script a bit more flexible

--- utilities/comparePlots.py
+++ utilities/comparePlots.py
@@ -8,27 +8,53 @@ import numpy as np
 import sys
 
 # Validate the input
-if len(sys.argv) != 5:
-    print("Usage: python comparePlots.py <label1> <label2> <csv1> <csv2>")
+if len(sys.argv) == 5 or len(sys.argv) == 3:
+    output_format = "show"
+elif len(sys.argv) == 4 or len(sys.argv) == 2:
+    output_format = "save"
+else: 
+    print("Usage: python comparePlots.py [GEN] <LABEL1> <LABEL2> <CSV1> <CSV2>")
     sys.exit(1)
 
 
-# Read in the csvs
-label1 = sys.argv[1]
-label2 = sys.argv[2]
-df1 = pd.read_csv(sys.argv[3])
-df2 = pd.read_csv(sys.argv[4])
+labels = []
+dfs = []
+
+if output_format == "save": 
+    gen = sys.argv[1]    
+    args = sys.argv[2:]
+else: 
+    args = sys.argv[1:]
 
 
+for (a, arg) in enumerate(args):
+    
+    if a < (len(args)/2):
+        labels.append(arg)
+    else:
+        dfs.append(pd.read_csv(arg))
 
-# Plot the data (first column is f1, second column is f2)
-plt.scatter(df1.iloc[:,1], df1.iloc[:,0]*-1,  label=label1)
-plt.scatter(df2.iloc[:,1], df2.iloc[:,0]*-1,  label=label2)
-plt.xlabel("f1")
-plt.ylabel("f2")
+for (d, df) in enumerate(dfs): 
+
+    # Plot the data (first column is f1, second column is f2)
+    plt.scatter(df.iloc[:,1], df.iloc[:,0]*-1,  label=labels[d])
+
+
+    
+plt.xlabel("Irrigation (mm)")
+plt.ylabel("Yield (kg/ha)")
 plt.legend()
-plt.show()
 
+if output_format == "save":
 
+    # turn gen into a string padded with four zeros
+    gen = gen.zfill(4)
+
+    # Save plot to /tmp
+    plt.savefig("/tmp/pareto%s.png" % gen)
+
+else: 
+
+    plt.show()
 
 
